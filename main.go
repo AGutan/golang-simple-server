b3c18c1dd2ab54a181673d9083085984d5da792d
alex <alex.gutan.m@gmail.com>
2021-04-22 15:59:16 +0300

    make todo

--- main.go
+++ main.go
@@ -1,17 +1,71 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
+	"log"
 	"net/http"
 )
 
-func main() {
-	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fs)
-	http.HandleFunc("/hello", hello)
-	http.ListenAndServe(":8080", nil)
+type Todo struct {
+	Todo string `json:"todo"`
 }
 
-func hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "hello\n")
+var todos []*Todo
+
+func main() {
+	router := mux.NewRouter()
+	// POST /todo
+	router.HandleFunc("/todo", newTodo).Methods(http.MethodPost)
+	// GET /todo
+	router.HandleFunc("/todo", getTodos).Methods(http.MethodGet)
+
+	// setup http server
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", 8080),
+		Handler: router,
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatalf("Could not start server")
+	}
+}
+
+func getTodos(w http.ResponseWriter, req *http.Request) {
+	// write success response
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(todos)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func newTodo(w http.ResponseWriter, req *http.Request) {
+	var todo Todo
+	err := json.NewDecoder(req.Body).Decode(&todo)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	todos = append(todos, &todo)
+	log.Printf("todo %v", todo)
+	// close body to avoid memory leak
+	err = req.Body.Close()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// write success response
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(todo)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
